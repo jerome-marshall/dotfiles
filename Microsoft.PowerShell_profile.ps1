f99d5e63898476251bc2f902ea22c891887a8b3f
Jerome Marshall <jeromemarshall0@gmail.com>
2022-12-29 13:08:56 +0530

    added starship and powershell config

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,30 @@
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+
+# $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+# if (Test-Path($ChocolateyProfile)) {
+#   Import-Module "$ChocolateyProfile"
+# }
+
+# Shows navigable menu of all options when hitting Tab
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+# Autocompletion for arrow keys
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+# NPM tab autocompletion
+# Execute -> Install-Module npm-completion -Scope CurrentUser
+Import-Module npm-completion
+
+# Git autocompletion
+Import-Module posh-git
+
+# Autosuggestions
+Import-Module PSReadLine
+Set-PSReadLineOption -PredictionSource History
+
+Invoke-Expression (&starship init powershell)
